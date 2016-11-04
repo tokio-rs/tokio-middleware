7821075760fa73f2762bd0e1af869bc5ee21a053
Carl Lerche <me@carllerche.com>
2016-11-04 09:45:07 -0700

    Revert "Update timeout.rs"
    This reverts commit eaad012239897114c06e3b6a943efc67f8fcfb82.
    
    Aka, what was I thinking with that commit?

--- src/timeout.rs
+++ src/timeout.rs
@@ -1,55 +1,30 @@
+use futures::Async;
+use tokio_service::Service;
+use tokio_timer::{self as timer, Timer};
+use std::time::Duration;
 
-/// Proposal: Standardize this error.
-///
-/// Custom errors should provide a conversion to SinkError.
-///
-/// For example:
-///     impl From<HttpError> for SinkError<HttpRequest, HttpError> {
-///     }
-///
-pub enum SinkError<T, E> {
-    NotReady(T),
-    Other(E),
+/// Abort requests that are taking too long
+#[derive(Clone)]
+pub struct Timeout<S> {
+    upstream: S,
+    timer: Timer,
+    duration: Duration,
 }
 
-/// Example middleware that uses SinkError
-impl<S> Service for Retry<S>
-    where SinkError<S::Request, S::Error>: From<S::Error>,
-{
-    type Request = S::Request;
-    type Error = S::Error;
-
-    fn call(&self, request: S::Request) -> impl Future<S::Response, S::Error> {
-        // Bump Arc count
-        let retry = self.clone();
-
-        self.inner.upstream.call(request).then(move |res| {
-            match res {
-                Ok(resp) => Ok(resp),
-                Err(e) => {
-                    match SinkError::from(e) {
-                        SinkError::NotReady(request) => {
-                            // Some global timer
-                            timer::sleep(Duration::from_secs(10))
-                                .and_then(|_| retry.call(request))
-                        }
-                        SinkError::Other(e) => Err(e),
-                    }
-                }
-            }
-        })
-    }
-
-    fn poll_ready(&self) -> Async<()> {
-        self.inner.upstream.poll_ready()
+impl<S> Timeout<S> {
+    /// Crate a new `Timeout` with the given `upstream` service.
+    ///
+    /// Requests will be limited to `duration` and aborted once the limit has
+    /// been reached.
+    pub fn new(upstream: S, timer: Timer, duration: Duration) -> Timeout<S> {
+        Timeout {
+            upstream: upstream,
+            duration: duration,
+            timer: timer,
+        }
     }
 }
 
-/// Any middleware generic over the error doesn't have to care. For example
-/// timout:
-///
-/// In this case, the timeout will proxy the upstream error or it will short
-/// circuit in the event of a timeout
 impl<S, E> Service for Timeout<S>
     where S: Service<Error = E>,
           E: From<timer::TimeoutError>,
